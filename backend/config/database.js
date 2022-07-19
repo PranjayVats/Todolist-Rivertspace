5ee33eba53db28aca978cf246311fe95d8365b74
Pranjay Vats <pranjayvats22062001@gmail.com>
2022-07-19 15:18:29 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,9 @@ const connectDatabase = async () => {
   mongoose.connect(
     process.env.DB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log(`Mongodb connected with server: ${data.connection.host}`)
+    () => {
+      console.log("Databse connected.");
+    }
   );
 };
 
